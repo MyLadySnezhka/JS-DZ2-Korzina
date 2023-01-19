04b1d88dcedeaa5ff521df71d050c8b8e225b5fb
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-01-19 04:45:07 +0200

    create food busket

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const nameCart = document.querySelector('h1');
+const selectCurrency = document.querySelector('.currency_select');
+const btnInc = document.querySelector('.butinc');
+const btnDec = document.querySelector('.butdec');
+const itemNum = document.querySelector('.item_num');
+const itemPrice = document.querySelector('.item_pr');
+const itemSum = document.querySelector('.item_sum');
+
+let num = 1;
+
+let itemPr = 0;
+
+nameCart.addEventListener('mouseenter', () => {
+    nameCart.innerHTML = 'Замовляйте!';
+})
+
+selectCurrency.addEventListener('change', (ev) => {
+    let curr = ev.target.value;
+    if (curr === 'usd') {itemSum.innerHTML = itemPr*itemNum.value*20};
+    if (curr === 'eur') {itemSum.innerHTML = itemPr*itemNum.value*10};
+    if (curr === 'grn') {itemSum.innerHTML = itemPr*itemNum.value};
+})
+
+btnDec.addEventListener('click', (ev) => {
+    num = num - 1;
+    if (num > 0) {
+    itemNum.value = num;
+    itemSum.innerHTML = num*itemPr;
+    }
+    else {
+        itemNum.value = 0;
+        itemSum.innerHTML = 'Недостатня кількість товарів!';
+        num = 0;
+        return;
+        };
+})
+
+btnInc.addEventListener('click', () => {
+    num = num + 1;
+    itemNum.value = num;
+    itemSum.innerHTML = num*itemPr;
+})
+
+itemPrice.addEventListener('change', (ev) => {
+    itemPr = ev.target.value;
+    itemSum.innerHTML = itemPr*itemNum.value;
+})
